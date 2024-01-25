ad7bd0d2da6f52c1ada70159cc3b4162e3ac71a6
Toheedullah-K3 <toheedullah002@gmail.com>
2024-01-25 19:23:18 +0500

    by unordered_set approach

---
+++ duplicateElement.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+bool findDuplicate(int arr[], int size) {
+    unordered_set<int> seen;
+
+    for (int i = 0; i < size; i++) {
+        if (seen.find(arr[i]) != seen.end()) {
+            return true;
+        }
+        seen.insert(arr[i]);
+    }
+
+    // No duplicates found
+    return false;
+}
+
+int main() {
+    int arr[] = {4, 2, 6, 8, 2, 3, 1};
+    int size = sizeof(arr) / sizeof(int);
+
+    bool duplicate = findDuplicate(arr, size);
+
+    if (duplicate) {
+        cout << "Duplicate items";
+    } else {
+        cout << "All items in the array are distinct";
+    }
+
+    return 0;
+}
